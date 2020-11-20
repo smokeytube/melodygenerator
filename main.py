cf23f670988f37fb75ff606ce523215dc9f588b9
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-11-19 20:38:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import winsound
+import random
+
+
+#notelengths
+sixteenth = 300
+eighth = 600
+dottedeight = 900
+quarter = 1200
+dottedquarter = 1800
+half = 2400
+dottedhalf = 3600
+whole = 4800
+
+#notepitches
+c4 = 261.6256
+cs4 = 277.1826
+d4 = 293.6648
+ds4 = 311.1270
+e4 = 329.6276
+f4 = 349.2282
+fs4 = 369.9944
+g4 = 391.9954
+gs4 = 415.3047
+a4 = 440
+
+
