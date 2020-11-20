dcc8b6258b189a3f63c226aacc3da539dc7b58c6
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-11-19 22:13:23 -0500

    checks for licc

--- main.py
+++ main.py
@@ -31,58 +31,7 @@ randpitchn = 0
 notelength = 0
 pitchn = c4
 
-def length():
-    if randlen == 1:
-        lengthn = sixteenth
-    elif randlen == 2:
-        lengthn = eighth
-    elif randlen == 3:
-        lengthn = dottedeight
-    elif randlen == 4:
-        lengthn = quarter
-    elif randlen == 5:
-        lengthn = dottedquarter
-    elif randlen == 6:
-        lengthn = half
-    elif randlen == 7:
-        lengthn = dottedhalf
-    elif randlen == 8:
-        lengthn = whole
-
-
-def pitch():
-    if randpitch == 1:
-        pitchn = c4
-    elif randpitch == 2:
-        pitchn = cs4
-    elif randpitch == 3:
-        pitchn = d4
-    elif randpitch == 4:
-        pitchn = ds4
-    elif randpitch == 5:
-        pitchn = e4
-    elif randpitch == 6:
-        pitchn = f4
-    elif randpitch == 7:
-        pitchn = fs4
-    elif randpitch == 8:
-        pitchn = g4
-    elif randpitch == 9:
-        pitchn = gs4
-    elif randpitch == 10:
-        pitchn = a4
-    elif randpitch == 11:
-        pitchn = as4
-    elif randpitch == 12:
-        pitchn = b4
-    elif randpitch == 13:
-        pitchn = c5
-
-
-
-while True:
-    randlen = random.randint(1, 8)
-    randpitch = random.randint(1, 13)
+def boop():
     if randlen == 1:
         lengthn = sixteenth
     elif randlen == 2:
@@ -131,3 +80,41 @@ while True:
     else:
         pitchn = c5
     winsound.Beep(pitchn, lengthn)
+
+while True:
+    randlen = random.randint(1, 8)
+    randpitch = random.randint(1, 13)
+    boop()
+
+
+    if randpitch == 2 and randlen == 2:
+        boop()
+        if randpitch == 3 and randlen == 2:
+            boop()
+            if randpitch == 4 and randlen == 2:
+                boop()
+                if randpitch == 5 and randlen == 2:
+                    boop()
+                    if randpitch == 3 and randlen == 4:
+                        boop()
+                        if randpitch == 1 and randlen == 2:
+                            boop()
+                            if randpitch == 2 and randlen == 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8:
+                                print ("lick")
+                                time.sleep(100000)
+                            else:
+                                pass
+                        else:
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
+            else:
+                pass
+        else:
+            pass
+    else:
+        pass
+
+        
